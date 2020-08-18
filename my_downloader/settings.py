36bd46b60a58323532fa510b32a9e87b371a0a23
Hari kishor jha <jhagopal706@gmail.com>
2020-08-18 07:07:33 +0530

    change in staticfiles_dirs

--- my_downloader/settings.py
+++ my_downloader/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "/static"),
     '/var/www/static/',
 ]
 #created manually
