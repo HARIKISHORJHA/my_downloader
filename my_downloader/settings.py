9e93254a47ca6703b85df472dfa83b7d5fc843c5
Hari kishor jha <jhagopal706@gmail.com>
2020-08-19 14:14:56 +0530

    changing logo settings

--- my_downloader/settings.py
+++ my_downloader/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 #created manually
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 #STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
