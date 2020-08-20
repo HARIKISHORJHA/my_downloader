5272b06507dd0c16b04d07a3288e14ab0b7b4a3e
Hari kishor jha <jhagopal706@gmail.com>
2020-08-20 09:28:24 +0530

    changing in urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.views.static import serve
+
 urlpatterns = [
     path("",views.home,name="home"), 
     path("home",views.home,name="home"),
     path("index",views.index,name='index'),
     path("service", views.service,name="service"),
-    
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+
 
 ]
