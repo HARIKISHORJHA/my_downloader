62e96c14bcf0f21ecf119c9b42b4bacaae9863fb
Hari kishor jha <jhagopal706@gmail.com>
2020-08-21 09:34:45 +0530

    unsplash image address

--- my_downloader/settings.py
+++ my_downloader/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'whitenoise.runserver_nostatic'
+    #'whitenoise.runserver_nostati'
     'django.contrib.staticfiles',
     
 
