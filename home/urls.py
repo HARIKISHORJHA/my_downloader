df319cad387ba85761cf4b0ca7ac8bd28c8f4bd9
Hari kishor jha <jhagopal706@gmail.com>
2020-08-21 07:24:06 +0530

    change in html

--- home/urls.py
+++ home/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 from django.views.static import serve
-from my_downloader import settings
+#from my_downloader import settings
 urlpatterns = [
     path("",views.home,name="home"), 
     path("home",views.home,name="home"),
     path("index",views.index,name='index'),
     path("service", views.service,name="service"),
-    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    
 
     #url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
 
